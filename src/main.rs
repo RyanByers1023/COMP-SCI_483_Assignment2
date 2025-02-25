352a0381b6da8d385e2c92a0b9909473610b3d22
Ryan Byers <ryanbyers1023@gmail.com>
2025-02-25 15:57:44 -0600

    create delete_a_letter. add is_palindrome variable scope comments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use std::ffi::c_int;
+
 fn main() {
 
 }
 
 pub fn is_palindrome(word: String) -> bool{
     //create version of word spelled forwards and one spelled backwards, filter numbers, convert all characters to lowercase
-    let mut letters = word.chars().filter(char::is_alphanumeric).map(char::to_ascii_lowercase); // -----------------+-'a
+    let mut letters = word.chars().filter(char::is_alphanumeric).map(char::to_ascii_lowercase);            // -----------------+-'a
     let mut letters_reverse = letters.rev().filter(char::is_alphanumeric).map(char::to_ascii_lowercase);   //-------------+-'b |
                                                                                                                                  //             |    |
     while let (Some(left), Some(right)) = (letters.next(), letters_reverse.next()){                                        //---+-d'--+-'c|    |
@@ -15,4 +17,15 @@ pub fn is_palindrome(word: String) -> bool{
     }                                                                                                                            //---+-----+   |    |
     true                                                                                                                         //             |    |
 }                                                                                                                                //-------------+----+
+
+pub fn delete_a_letter(word: String, index: usize) -> String{
+    let mut letters: Vec<char> = word.chars().collect(); //convert to mutable data type
+
+    //do not perform deletion at OOB index
+    if index < letters.len() {
+        letters.remove(index);
+    }
+
+    letters.into_iter().collect() //convert back to string, return changed string
+}
 
